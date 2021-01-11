a444f8aa2d70771ba002a092c85e1e80f35df787
Jefferson Calmon <jeffjeff0197@gmail.com>
2021-01-11 16:51:58 -0300

    feat: Add full project

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+document.addEventListener('scroll', () => {
+    const button = document.querySelector('#back-to-top');
+
+    if (window.scrollY > 550) {
+        button.classList.add('active');
+    } else {
+        button.classList.remove('active');
+    }
+});
+
+function toggleElementsById(id) {
+    const ids = typeof id !== 'string' ? [ ...id ] : [ id ];
+
+    for (id of ids) {
+        const element = document.getElementById(id);
+
+        element.classList.toggle('active');
+    }
+}
